2070aa59d32df7e00cc8d1cf099a8eadea2f0e83
golden race <jeison@goldenrace.com>
2023-05-27 22:16:52 -0500

    add order

--- src/components/turn/homework/homework.js
+++ src/components/turn/homework/homework.js
@@ -1,10 +1,9 @@
-import './homework.css'
-import moment from 'moment';
+import "./homework.css";
+import moment from "moment";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
 export default function Homeworks({ id, text, deleteTurn, turnDate }) {
-
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id });
 
@@ -13,23 +12,21 @@ export default function Homeworks({ id, text, deleteTurn, turnDate }) {
     transition,
   };
 
-
-    return (
-        <div style={style}
-           ref={setNodeRef}
-               {...attributes}
-               {...listeners}>
-            <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-                rel="stylesheet" />
-            <div className={'turn-container'}>
-                <div className="turn-text">
-                    {text}
-                    <div className='text-date '>{moment(turnDate).format('LL')}</div>
-                </div>
-                {/* <div className='icons' onMouseDown={() => deleteTurn(id)}>
-                <i className="material-icons">delete</i>
-                </div> */}
-            </div>
+  return (
+    <div style={style} ref={setNodeRef} {...attributes} {...listeners}>
+      <link
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet"
+      />
+      <div className={"turn-container"}>
+        <div className="turn-text">
+          {text}
+          <div className="text-date ">{moment(turnDate).format("HH:mm:ss")}</div>
         </div>
-    )
+        <div className="icons" onMouseDown={() => deleteTurn(id)}>
+          <i className="material-icons">delete</i>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/turn/turn.js
+++ src/components/turn/turn.js
@@ -1,23 +1,21 @@
-import { auth } from '../../firebase';
+import { auth } from "../../firebase";
 
-import './turn.css'
-import FormTurn from './formTurn/formTurn';
-import { useEffect, useState } from 'react';
-import Homeworks from './homework/homework';
+import "./turn.css";
+import FormTurn from "./formTurn/formTurn";
+import { useEffect, useState } from "react";
+import Homeworks from "./homework/homework";
 //import { ToastContainer, toast } from 'react-toastify';
-import { DndContext, closestCenter} from "@dnd-kit/core";
+import { DndContext, closestCenter } from "@dnd-kit/core";
 import {
   SortableContext,
   arrayMove,
-  verticalListSortingStrategy
+  verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
 
 export default function Turn() {
-
   const [turns, setTurns] = useState([]);
   const apiUrl = process.env.REACT_APP_API;
   const [isLoading, setIsLoading] = useState(false);
-  const [turnEdit, setturnEdit] = useState(null);
   const [idFirebaseUser, setIdFirebaseUser] = useState(null);
   const [turnResponse, setTurnResponse] = useState(null);
 
@@ -32,19 +30,18 @@ export default function Turn() {
   }, [apiUrl]);
 
   useEffect(() => {
-     const getturnById = async () => {
-       try {
-        console.log('actualizando use efect');
-         setIsLoading(true);
-         const responseturnByUser = await fetch(`${apiUrl}/turns`);
-         const responseturnByUserJson = await responseturnByUser.json();
-         setTurns(responseturnByUserJson);
-         setIsLoading(false);
-       } catch (error) {
-         //console.error(error);
-       }
-     }
-     getturnById();
+    const getturnById = async () => {
+      try {
+        setIsLoading(true);
+        const responseturnByUser = await fetch(`${apiUrl}/turns`);
+        const responseturnByUserJson = await responseturnByUser.json();
+        setTurns(responseturnByUserJson);
+        setIsLoading(false);
+      } catch (error) {
+        //console.error(error);
+      }
+    };
+    getturnById();
   }, [idFirebaseUser, turnResponse]);
 
   /**
@@ -52,106 +49,96 @@ export default function Turn() {
    */
   const saveTurn = async (turn) => {
     const responseAddTurn = await fetch(`${apiUrl}/turns`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(turn)
+      body: JSON.stringify(turn),
     });
     return await responseAddTurn.json();
-  }
-
+  };
 
   const addTurn = async (turn) => {
     if (turn) {
-        const responseTurn = await saveTurn(turn);
-        setTurnResponse(responseTurn);
-        //toast.success('Agregaste una tarea', { autoClose: 1000 , position: toast.POSITION.TOP_CENTER });
-      }
-  }
+      const responseTurn = await saveTurn(turn);
+      setTurnResponse(responseTurn);
+      //toast.success('Agregaste una tarea', { autoClose: 1000 , position: toast.POSITION.TOP_CENTER });
+    }
+  };
 
   const deleteTurn = async (id) => {
     const deleteTurn = await fetch(`${apiUrl}/turns/${id}`, {
-      method: 'DELETE',
+      method: "DELETE",
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
     const responseDeleteTurn = deleteTurn.json();
     setTurnResponse(responseDeleteTurn);
     //toast.error('Eliminaste una tarea', { autoClose: 1000 , position: toast.POSITION.TOP_CENTER });
-  }
+  };
 
-  // const completeTurn = async (id) => {
-  //   const turn = turns.find(turn => turn.id === id);
-  //   turn.completed = !turn.completed;
+  const handleDragEnd = async (event) => {
+    try {
+      const { active, over } = event;
+      console.log("active.id", active.id);
+      console.log("over.id", over.id);
+      const oldIndex = turns.findIndex((turn) => turn.id === active.id);
+      const newIndex = turns.findIndex((turn) => turn.id === over.id);
+      console.log("oldIndex", oldIndex);
+      console.log("newIndex", newIndex);
+      const arrayOrder = arrayMove(turns, oldIndex, newIndex);
 
-  //   const UpdateTurn = await fetch(`${apiUrl}/turns/${turn.id}`, {
-  //     method: 'PUT',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify(turn)
-  //   });
-  //   const responseUpdateTurn = UpdateTurn.json();
-  //   setTurnResponse(responseUpdateTurn);
-  //   if (turn.completed) {
-  //     //toast.success('Completaste una tarea', { autoClose: 1000 ,  position: toast.POSITION.TOP_CENTER });
-  //   } else {
-  //     //toast.warning('Desmarcaste una tarea', { autoClose: 1000 ,  position: toast.POSITION.TOP_CENTER });
-  //   }
-  // }
-
-  const handleDragEnd =async (event) =>{
-    const {active, over} = event;
-    console.log('active->',active.id);
-    console.log('over->',over.id);
-    if (!active.id !== over.id) {
-        const oldIndex = turns.findIndex(turn => turn.id === active.id);
-        const newIndex = turns.findIndex(turn => turn.id === over.id);
-        console.log('oldIndex->',oldIndex);
-        console.log('newIndex->',newIndex);
-        const arrayOrder = arrayMove(turns, oldIndex ,newIndex)
-
-        const updateArray = await fetch(`${apiUrl}/turns/order/create`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(arrayOrder)
+      if (arrayOrder.length > 0) {
+        let count = 1;
+        arrayOrder.forEach((element) => {
+          return (element.order = count++);
         });
-        const responseUpdateTask = updateArray.json();
-        setTurnResponse(responseUpdateTask);
-        
-        
-    };
-  }
-   
 
+        if (active.id !== over.id) {
+          const updateArray = await fetch(`${apiUrl}/turns/order/create`, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(arrayOrder),
+          });
+          const responseUpdateTask = updateArray.json();
+          setTurnResponse(responseUpdateTask);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <div className='container-turn'>
-      <DndContext
-          collisionDetection={closestCenter}
-          onDragEnd={handleDragEnd}
-        >
-      {isLoading ? (<p>Cargando informacion...</p>) : (<><FormTurn addTurn={addTurn} turnEdit={turnEdit}></FormTurn>
-        <div className='turn-list-content'>
-        <SortableContext
-            items={turns} strategy={verticalListSortingStrategy}>
-          {
-            turns.map((turn) =>
-              <Homeworks
-                key={turn.id}
-                id={turn.id}
-                text={turn.name}
-                turnDate={turn.date_register}
-                deleteTurn={deleteTurn}/>
-                )
-          }
-          </SortableContext>
-        </div>
-      </>)}
+    <div className="container-turn">
+      <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+        {isLoading ? (
+          <p>Cargando informacion...</p>
+        ) : (
+          <>
+            <FormTurn addTurn={addTurn}></FormTurn>
+            <div className="turn-list-content">
+              <SortableContext
+                items={turns}
+                strategy={verticalListSortingStrategy}
+              >
+                {turns.map((turn) => (
+                  <Homeworks
+                    key={turn.id}
+                    id={turn.id}
+                    text={turn.name}
+                    turnDate={turn.date_register}
+                    deleteTurn={deleteTurn}
+                  />
+                ))}
+              </SortableContext>
+            </div>
+          </>
+        )}
       </DndContext>
-    </div>)
+    </div>
+  );
 }
