541bab8de6ad880f820b516623b02fd2518ddff0
jeison sarria <jeison@goldenrace.com>
2023-07-01 20:16:35 -0500

    add email validation

--- src/components/customer/customer.js
+++ src/components/customer/customer.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Formturn from "../turn/formTurn/formTurn";
 import { useEffect } from "react";
 import "./customer.css";
+import { ToastContainer, toast } from "react-toastify";
 
 export default function Customer() {
   const [customerResponse, setCustomerResponse] = useState(null);
@@ -38,10 +39,17 @@ export default function Customer() {
     if (customer) {
       const responseCustomer = await saveCustomer(customer);
       setCustomerResponse(responseCustomer);
-      // toast.success("Agregaste un cliente", {
-      //   autoClose: 1000,
-      //   position: toast.POSITION.TOP_CENTER,
-      // });
+      if (!responseCustomer) {
+         toast.error("Ya aexiste un cliente con el email", {
+           autoClose: 1000,
+           position: toast.POSITION.TOP_CENTER,
+         });
+      }else{
+        toast.success("Agregaste un cliente", {
+          autoClose: 1000,
+          position: toast.POSITION.TOP_CENTER,
+        });
+      }
     }
   };
 
@@ -64,20 +72,18 @@ export default function Customer() {
     return response.id_users;
   };
 
-  
   return (
     <>
+     <ToastContainer />
       <Formturn addTurn={addCustomer} />
-      
-        {customers.map((customer) => (
-         <div class="card">
-         <div class="card-body">
-           <h5 class="card-title">{customer.name}</h5>
-           <p class="card-text">{customer.email}</p>
-         </div>
-       </div>
-        ))}
-      
+      {customers.map((customer) => (
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{customer.name}</h5>
+            <p class="card-text">{customer.email}</p>
+          </div>
+        </div>
+      ))}
     </>
   );
 }
--- src/components/turn/formTurn/formTurn.js
+++ src/components/turn/formTurn/formTurn.js
@@ -1,71 +1,78 @@
 import { useEffect, useState } from "react";
-import './formTurn.css';
+import "./formTurn.css";
 
 export default function Formturn({ addTurn }) {
+  const [input, setInput] = useState({
+    name: "",
+    email: "",
+  });
+  const [isEnabledButton, setIsEnabledButton] = useState(true);
 
-    const [input, setInput] = useState({
-        name: "",
-        email: ""
-    });
-    const [isEnabledButton, setIsEnabledButton] = useState(true);
-    
-
-    useEffect(() => {
-        if (input.name !== '' && input.email !== '') {
-            setIsEnabledButton(false)
-        }
-        if (input.name === '' || input.email === '') {
-            setIsEnabledButton(true)
-        }
-    }, [input])
-
-    
-    const handleSend = e => {
-        e.preventDefault();
-        if (input.name !== '' && input.email) {
-            addTurn(createTurn(), 'turnCustomer');
-            setInput({});
-            setIsEnabledButton(true);
-        }
+  useEffect(() => {
+    if (input.name !== "" && input.email !== "") {
+      setIsEnabledButton(false);
     }
-
-    const createTurn = () => {
-            return {
-                name: input.name.toLowerCase(),
-                email: input.email.toLowerCase()
-        }
+    if (input.name === "" || input.email === "") {
+      setIsEnabledButton(true);
     }
+  }, [input]);
 
-    const handleName = (event) => {
-        setInput({ ...input, name: event.target.value });
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@gmail\.com$/i;
+    if (emailRegex.test(email)) {
+      return true;
+    } else {
+        setInput({ ...input, email: 'El correo electronico es incorrecto' });
+      return false;
     }
-    const handleEmail = (event) => {
-        setInput({ ...input, email: event.target.value });
-    }
+  };
 
-    return (
-        <form className="turn-form" onSubmit={handleSend}>
-            <input
-                className='turn-input'
-                type='text'
-                placeholder='Agrega nombre y apellido'
-                name='texto'
-                autoComplete="off"
-                value={input.name ?? ''}
-                onChange={handleName}
-            />
-            <input
-                className='turn-input'
-                type='text'
-                placeholder='Ingresa email'
-                name='texto'
-                autoComplete="off"
-                value={input.email ?? ''}
-                onChange={handleEmail}
-            />
-            <button  hidden={isEnabledButton}  className="btn-sm rounded turn-botton" >
-                Crear turno
-            </button>
-        </form>
-    )
+  const handleSend = (e) => {
+    e.preventDefault();
+    if (input.name !== "" && validateEmail(input.email)) {
+      addTurn(createTurn(), "turnCustomer");
+      setInput({});
+      setIsEnabledButton(true);
+    }
+  };
+
+  const createTurn = () => {
+    return {
+      name: input.name.toLowerCase(),
+      email: input.email.toLowerCase(),
+    };
+  };
+
+  const handleName = (event) => {
+    setInput({ ...input, name: event.target.value });
+  };
+  const handleEmail = (event) => {
+    setInput({ ...input, email: event.target.value });
+  };
+
+  return (
+    <form className="turn-form" onSubmit={handleSend}>
+      <input
+        className="turn-input"
+        type="text"
+        placeholder="Agrega nombre y apellido"
+        name="texto"
+        autoComplete="off"
+        value={input.name ?? ""}
+        onChange={handleName}
+      />
+      <input
+        className="turn-input"
+        type="text"
+        placeholder="Ingresa email"
+        name="texto"
+        autoComplete="off"
+        value={input.email ?? ""}
+        onChange={handleEmail}
+      />
+      <button hidden={isEnabledButton} className="btn-sm rounded turn-botton">
+        Crear turno
+      </button>
+    </form>
+  );
 }
