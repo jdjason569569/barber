25ca209390061ff59d39ffe33f77c8f0c4244075
jeison sarria <jeison@goldenrace.com>
2023-09-03 21:15:39 -0500

    fix: add styles to turnComponent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { MyRoutes } from './routes/routes';
 
 function App() {
+  useEffect(() => {
+    // Agrega un listener para el evento de desplazamiento (scroll)
+    document.addEventListener('touchmove', handleTouchMove, { passive: false });
+
+    // Limpia el listener cuando el componente se desmonta
+    return () => {
+      document.removeEventListener('touchmove', handleTouchMove);
+    };
+  }, []);
+  const handleTouchMove = (e) => {
+    // Evitar el comportamiento predeterminado del navegador
+    e.preventDefault();
+  };
   return (
     <div className="App">
       <MyRoutes />
