b23eac34c1511d1a508e55571ce7c92ba981eccb
jeison sarria <jeison@goldenrace.com>
2024-02-06 23:28:39 -0500

    toast

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,31 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { auth } from "../firebase";
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import Login from "../components/login/login";
-import SignUp from "../components/signUp/signUp";
-import Home from "../components/home/home";
-import { ProtectedRoute } from "../components/shared/protectedRoute";
+import Login from '../components/login/login';
+import SignUp from '../components/signUp/signUp';
+import Home from '../components/home/home';
+import { ProtectedRoute } from '../components/shared/protectedRoute';
 
 export function MyRoutes() {
-  const [userName, setUserName] = useState(null);
-
-  useEffect(() => {
-    auth.onAuthStateChanged((user) => {
-      user ? setUserName(user.displayName) : setUserName(null);
+    const [userName, setUserName] = useState(null);
+    
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            user ? setUserName(user.displayName) : setUserName(null);
+        }); 
     });
-  });
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="*" element={<Login />} replace></Route>
-        <Route path="/" element={<Login />} replace></Route>
-        <Route element={<ProtectedRoute name={userName} />}>
-          <Route path="/home" element={<Home name={userName} />}></Route>
-        </Route>
-        <Route path="/signup" element={<SignUp />}></Route>
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<Navigate to="/login" />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/signup' element={<SignUp />} />
+                <Route element={<ProtectedRoute name={userName} />}>
+                    <Route path='/home' element={<Home name={userName} />} />
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
