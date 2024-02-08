6489ba6c23bfe857b0ec21c35284e5a08422be3c
jeison sarria <jeison@goldenrace.com>
2024-02-07 20:04:25 -0500

    test

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,35 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { auth } from "../firebase";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import Login from '../components/login/login';
-import SignUp from '../components/signUp/signUp';
-import Home from '../components/home/home';
-import { ProtectedRoute } from '../components/shared/protectedRoute';
-
-
+import Login from "../components/login/login";
+import SignUp from "../components/signUp/signUp";
+import Home from "../components/home/home";
+import { ProtectedRoute } from "../components/shared/protectedRoute";
 
 export function MyRoutes() {
+  const [userName, setUserName] = useState(null);
 
-    const [userName, setUserName] = useState(null);
-    
-    useEffect(() => {
-        auth.onAuthStateChanged((user) => {
-            user ? setUserName(user.displayName) : setUserName(null);
-        }); 
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      user ? setUserName(user.displayName) : setUserName(null);
     });
+  });
 
-    return (
-         <Router>
-             <Routes>
-                 <Route  path='/' element={<Login />}></Route>
-                 <Route element={<ProtectedRoute name={userName} />}>
-                     <Route exact path='/home' element={<Home name={userName} />}></Route>
-                 </Route>
-                 <Route exact path='/signup'  element={<SignUp />}></Route>
-                 <Route  path='*' element={<Login />}></Route>
-             </Routes>
-         </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/home" element={<Home name={userName} />}></Route>
+        <Route path="/" element={<Login />}></Route>
+
+        <Route exact path="/signup" element={<SignUp />}></Route>
+        <Route path="*" element={<Login />}></Route>
+      </Routes>
+    </Router>
+  );
 }
-
